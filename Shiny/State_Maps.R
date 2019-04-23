22e6d5389c562f085b52e59c740e09dad232bb72
nnl101 <namsonngole@gmail.com>
2019-04-23 18:30:58 -0400

    updated slides

--- Shiny/State_Maps.R
+++ Shiny/State_Maps.R
@@ -9,6 +9,8 @@ library(choroplethr)      # Creating Choropleth Maps in R
 library(choroplethrMaps)  # Maps used by the choroplethr package
 library(dplyr)
 library(data.table)
+library(rsconnect)
+
 
 dat <- read.csv(url("https://raw.githubusercontent.com/Victoriaxyan/machine-churning/master/Data/State_Data/ILINet.csv"))
 dat$month<-lubridate::month(as.Date(paste0(dat$YEAR, "-", dat$WEEK, "-", 10), 
@@ -62,8 +64,7 @@ server <- function(input, output) {
     data_20$value <- data_20[, input$select]
     state_choropleth(data_20,
                      title = input$select,
-                     num_colors    = 7,
-                     zoom          = continental_us_states)
+                     num_colors    = 7)
   })
   
   output$table <- renderDataTable({
