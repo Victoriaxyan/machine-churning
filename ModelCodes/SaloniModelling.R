c1b55efa19405ed311b01c1748e6a8402a9f2a3e
Saloni Mohan <sm4632@columbia.edu>
2019-05-09 09:22:11 -0400

    Final report

--- ModelCodes/SaloniModelling.R
+++ ModelCodes/SaloniModelling.R
@@ -2,70 +2,304 @@
 library(forecast)
 library(data.table)
 library(DT)
+library(datetime)
+
+library(keras)
+install_keras()
+
+# Core Tidyverse
+library(tidyverse)
+library(glue)
+library(forcats)
+
+# Time Series
+library(timetk)
+library(tidyquant)
+library(tibbletime)
+
+# Visualization
+library(cowplot)
+
+# Preprocessing
+library(recipes)
+
+# Sampling / Accuracy
+library(rsample)
+library(yardstick) 
+
+# Modeling
+library(keras)
 
 
-visits <- read.csv("../UncleanedData/flu_view_line(Topright  Chart)_national.csv")
+
+visits <- fread("https://raw.githubusercontent.com/Victoriaxyan/machine-churning/master/Data/flu_view_line(Topright%20%20Chart)_national.csv")
 
 ######################
 ####Data Cleaning#####
 ######################
 
-#----Converting weekly records into monthly------
-visits$month<-lubridate::month(as.Date(paste0(visits$YEAR, "-", visits$WEEK, "-", 10), 
-                                       format = "%Y-%U-%u"))
-visits$date <- paste(visits$YEAR,visits$month,sep="-")
-visits <- as.data.table(visits)
-head(visits)
 
-#-----preparing data for modeling------
+visits_ili <- visits[,.(ili_percent= (ILITOTAL*100/TOTAL.PATIENTS))]
+visits_with_ilipercent <- cbind(visits,visits_ili)
 
-#Part 1 (time series data): percentage of visits for ILI, National
-ili.percent <- visits[,.(percent = ILITOTAL/TOTAL.PATIENTS)]
 
-#Part 2 (age group data): ILI visits according to age group
-age.groups <- names(visits)[c(4:6,8:9)]
-ili.age <- visits[,c(2:6,8:9,15:16)]
-head(ili.age)
 
-#################################
-####Exploratory Data Analysis####
-#################################
+visits_removing53rdweek <- visits_with_ilipercent[WEEK >=1 & WEEK <=52,]
+visits_for_ts <- visits_removing53rdweek[,c(2,3,15)]
+ili <- visits_for_ts[,c(3)]
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# time series model for percentage of total ILI visits, weekly
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-ts.ili.percent <- ts(ili.percent, frequency = 52)
+ts.ili.percent <- ts(ili, frequency = 52, start=c(2010, 1), end=c(2019, 12), frequency=52)
 
-#fitting a model using auto.arima
-m.auto <- auto.arima(ts.ili.percent)
-m.auto #best model: (2,0,1)(0,1,1)[52]
-arma <- m.auto$arma #subtracts the orders of SARIMA model
+train = window(ts.ili.percent,end=c(2018,39))
+test = window(ts.ili.percent, start=c(2018,40))
+length(test)
 
-#forecast
-m <- arima(ts.ili.percent, order = c(arma[1], arma[6], arma[2]), 
-           seasonal = list(order = c(arma[3], arma[7], arma[4]), period = 52)) 
-predictions <- predict(m, n.ahead = 52)$pred
+model_auto = auto.arima(train)
+accuracy(model_auto)
+model1_forecast = forecast(model_auto,h=25)
+accuracy(forecast(model_auto, h = 25), ts.ili.percent)
 
-par(mfrow = c(2,1))
-plot(1:(length(ts.ili.percent) + length(predictions)), 
-     c(ts.ili.percent, predictions), type = 'l', col = 1, 
-     xlab = "week number", ylab = "ILI visits percentage",
-     main = "predictions of ILI visits percentage")
-points((length(ts.ili.percent) + 1) : (length(ts.ili.percent) + length(predictions)), 
-       predictions, type = 'l', col = 2)
+## LSTM -- Model1
 
-plot(1:length(predictions), 
-     predictions, type = 'l', col = 2, 
-     xlab = "week number starting from 13th week, 2019", 
-     ylab = "ILI visits percentage",
-     main = "predictions of ILI visits percentage")
+diffed = diff(ts.ili.percent, differences = 1)
 
-# > Conclusion: Strong Seasonality in ILI. Percentage of patients visiting the hospital for ILI may drop from now through summer, and getting back in the fall.
+lag_transform <- function(x, k= 1){
+  
+  lagged =  c(rep(NA, k), x[1:(length(x)-k)])
+  DF = as.data.frame(cbind(lagged, x))
+  colnames(DF) <- c( paste0('x-', k), 'x')
+  DF[is.na(DF)] <- 0
+  return(DF)
+}
+supervised = lag_transform(diffed, 1)
+head(supervised)
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ILI visits: Distribution by Age Groups
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#### Twitter
 
+library(ggmap)
+library(ggplot2)
+library(rtweet)
+library(maps)
+library(stringr)
+
+N = nrow(supervised)
+n = round(N *0.7, digits = 0)
+train = supervised[1:n, ]
+test  = supervised[(n+1):N,  ]
+
+scale_data = function(train, test, feature_range = c(0, 1)) {
+  x = train
+  fr_min = feature_range[1]
+  fr_max = feature_range[2]
+  std_train = ((x - min(x) ) / (max(x) - min(x)  ))
+  std_test  = ((test - min(x) ) / (max(x) - min(x)  ))
+  
+  scaled_train = std_train *(fr_max -fr_min) + fr_min
+  scaled_test = std_test *(fr_max -fr_min) + fr_min
+  
+  return( list(scaled_train = as.vector(scaled_train), scaled_test = as.vector(scaled_test) ,scaler= c(min =min(x), max = max(x))) )
+  
+}
+
+
+Scaled = scale_data(train, test, c(-1, 1))
+
+y_train = Scaled$scaled_train[, 2]
+x_train = Scaled$scaled_train[, 1]
+
+y_test = Scaled$scaled_test[, 2]
+x_test = Scaled$scaled_test[, 1]
+
+
+invert_scaling = function(scaled, scaler, feature_range = c(0, 1)){
+  min = scaler[1]
+  max = scaler[2]
+  t = length(scaled)
+  mins = feature_range[1]
+  maxs = feature_range[2]
+  inverted_dfs = numeric(t)
+  
+  for( i in 1:t){
+    X = (scaled[i]- mins)/(maxs - mins)
+    rawValues = X *(max - min) + min
+    inverted_dfs[i] <- rawValues
+  }
+  return(inverted_dfs)
+}
+
+
+## LSTM - MODELLING
+
+dim(x_train) <- c(length(x_train), 1, 1)
+
+# specify required arguments
+X_shape2 = dim(x_train)[2]
+X_shape3 = dim(x_train)[3]
+batch_size = 1                # must be a common factor of both the train and test samples
+units = 1                     # can adjust this, in model tuninig phase
+
+#=========================================================================================
+
+model <- keras_model_sequential() 
+model%>%
+  layer_lstm(units, batch_input_shape = c(batch_size, X_shape2, X_shape3), stateful= TRUE)%>%
+  layer_dense(units = 1)
+
+
+model %>% compile(
+  loss = 'mean_squared_error',
+  optimizer = optimizer_adam( lr= 0.02, decay = 1e-6 ),  
+  metrics = c('accuracy')
+)
+
+summary(model)
+
+Epochs = 25   
+for(i in 1:Epochs ){
+  model %>% fit(x_train, y_train, epochs=1, batch_size=batch_size, verbose=1, shuffle=FALSE)
+  model %>% reset_states()
+}
+
+
+L = length(x_test)
+scaler = Scaled$scaler
+predictions = numeric(L)
+
+for(i in 1:L){
+  X = x_test[i]
+  dim(X) = c(1,1,1)
+  yhat = model %>% predict(X, batch_size=batch_size)
+  # invert scaling
+  yhat = invert_scaling(yhat, scaler,  c(-1, 1))
+  # invert differencing
+  yhat  = yhat + ts.ili.percent[(n+i)]
+  # store
+  predictions[i] <- yhat
+}
+
+
+actual <- ili.percent[c(310:441),]
+
+RMSE <- (sqrt(mean((predictions - actual$ili_percent)^2)))
+RMSE
+
+
+## LSTM -- Model2
+
+diffed = diff(ts.ili.percent, differences = 1)
+
+lag_transform <- function(x, k= 1){
+  
+  lagged =  c(rep(NA, k), x[1:(length(x)-k)])
+  DF = as.data.frame(cbind(lagged, x))
+  colnames(DF) <- c( paste0('x-', k), 'x')
+  DF[is.na(DF)] <- 0
+  return(DF)
+}
+supervised = lag_transform(diffed, 1)
+head(supervised)
+
+
+
+
+train = supervised[1:415, ]
+test  = supervised[416:440,  ]
+
+scale_data = function(train, test, feature_range = c(0, 1)) {
+  x = train
+  fr_min = feature_range[1]
+  fr_max = feature_range[2]
+  std_train = ((x - min(x) ) / (max(x) - min(x)  ))
+  std_test  = ((test - min(x) ) / (max(x) - min(x)  ))
+  
+  scaled_train = std_train *(fr_max -fr_min) + fr_min
+  scaled_test = std_test *(fr_max -fr_min) + fr_min
+  
+  return( list(scaled_train = as.vector(scaled_train), scaled_test = as.vector(scaled_test) ,scaler= c(min =min(x), max = max(x))) )
+  
+}
+
+
+Scaled = scale_data(train, test, c(-1, 1))
+
+y_train = Scaled$scaled_train[, 2]
+x_train = Scaled$scaled_train[, 1]
+
+y_test = Scaled$scaled_test[, 2]
+x_test = Scaled$scaled_test[, 1]
+
+
+invert_scaling = function(scaled, scaler, feature_range = c(0, 1)){
+  min = scaler[1]
+  max = scaler[2]
+  t = length(scaled)
+  mins = feature_range[1]
+  maxs = feature_range[2]
+  inverted_dfs = numeric(t)
+  
+  for( i in 1:t){
+    X = (scaled[i]- mins)/(maxs - mins)
+    rawValues = X *(max - min) + min
+    inverted_dfs[i] <- rawValues
+  }
+  return(inverted_dfs)
+}
+
+
+## LSTM - MODELLING
+
+dim(x_train) <- c(length(x_train), 1, 1)
+
+# specify required arguments
+X_shape2 = dim(x_train)[2]
+X_shape3 = dim(x_train)[3]
+batch_size = 1                # must be a common factor of both the train and test samples
+units = 1                     # can adjust this, in model tuninig phase
+
+#=========================================================================================
+
+model <- keras_model_sequential() 
+model%>%
+  layer_lstm(units, batch_input_shape = c(batch_size, X_shape2, X_shape3), stateful= TRUE)%>%
+  layer_dense(units = 1)
+
+
+model %>% compile(
+  loss = 'mean_squared_error',
+  optimizer = optimizer_adam( lr= 0.02, decay = 1e-6 ),  
+  metrics = c('accuracy')
+)
+
+summary(model)
+
+Epochs = 25   
+for(i in 1:Epochs ){
+  model %>% fit(x_train, y_train, epochs=1, batch_size=batch_size, verbose=1, shuffle=FALSE)
+  model %>% reset_states()
+}
+
+
+L = length(x_test)
+scaler = Scaled$scaler
+predictions = numeric(L)
+
+for(i in 1:L){
+  X = x_test[i]
+  dim(X) = c(1,1,1)
+  yhat = model %>% predict(X, batch_size=batch_size)
+  # invert scaling
+  yhat = invert_scaling(yhat, scaler,  c(-1, 1))
+  # invert differencing
+  yhat  = yhat + ts.ili.percent[(n+i)]
+  # store
+  predictions[i] <- yhat
+}
+
+
+actual <- ili.percent[c(417:441),]
+
+RMSE <- (sqrt(mean((predictions - actual$ili_percent)^2)))
+RMSE
 
 
